60dfcb24b1cdf5c4389d30985b6bc67752bbb210
OceanicRavi <18ravibhatt@gmail.com>
2018-10-02 13:10:07 +1300

    Update Helper.cs

--- GenerateUniqueNumbers/GenerateUniqueNumbers/Helper.cs
+++ GenerateUniqueNumbers/GenerateUniqueNumbers/Helper.cs
@@ -6,14 +6,23 @@ using System.Threading.Tasks;
 
 namespace GenerateUniqueNumbers
 {
+    /*
+     * Helper class to provide basic functionalities.
+     * Use this class to perfom repeated tasks.
+     */
     public static class Helper
     {
+        /// <summary>
+        /// Function to get random number from the range of minValue and maxValue-1.
+        /// </summary>
         public static int RandomNumberFromRange(int minValue,int maxValue)
         {
             Random rdm = new Random();
             return rdm.Next(minValue, maxValue);
         }
-
+        /// <summary>
+        /// Function to print each item of elements on new line.
+        /// </summary>
         public static void PrintByLine<T>(List<T> elements)
         {
             foreach(var item in elements)
